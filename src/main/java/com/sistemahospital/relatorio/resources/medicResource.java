77e83d5b1eaf2b5c6a71e4a686b38a7c8e13b43c
Ramon Marino <ramonmarino12@gmail.com>
2023-08-11 10:14:50 -0300

    medic e resources

---
+++ src/main/java/com/sistemahospital/relatorio/resources/medicResource.java
@@ -0,0 +1,23 @@
+package com.sistemahospital.relatorio.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sistemahospital.relatorio.entities.medic;
+
+@RestController
+@RequestMapping(value="/medic")
+public class medicResource {
+	
+	@GetMapping
+	public ResponseEntity<medic> findAll(){
+		medic m = new medic(1L,"Pedro", "pedro@gmail.com", "45678901","12345");
+		return ResponseEntity.ok().body(m);
+		
+	}
+	
+	
+
+}
